044fee9a6da892038161bc3d2afde9414ac1c850
coriovc <victororio2910@gmail.com>
2020-09-11 07:48:54 -0400

    update

---
+++ js/validator-contact.js
@@ -0,0 +1,30 @@
+$( document ).ready( function () {
+      $( "#contact_form" ).validate( {
+        rules: {
+          name: "required",
+          email: {required: true, email: true},
+          phone: {required: true, minlength: 10},          
+          message: "required"
+          },
+        messages: {
+          name: "Please enter your name",
+          email:{required: "Please enter an email",
+                email:"The email format is not valid EJ: example@mail.com"},
+          phone: {required: "Please enter a phone number.",
+                  minlength: "Enter the area code without the 15: EJ: 1145652798."},          
+          message: "Please write a message."
+          },
+        errorElement: "span",
+        errorPlacement: function ( error, element ) {
+          // Add the `invalid-feedback` class to the error element
+          error.addClass( "invalid-feedback" );
+          error.insertAfter( element );
+        },
+        highlight: function ( element, errorClass, validClass ) {
+          $( element ).addClass("is-invalid").removeClass("is-valid");
+        },
+        unhighlight: function (element, errorClass, validClass) {
+          $( element ).addClass("is-valid").removeClass("is-invalid");
+        }
+      } );
+    } );
