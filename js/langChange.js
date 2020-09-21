c97000f4361bb5c0b1fc6b722d82da1584330a76
Victor corio <65185738+coriovc@users.noreply.github.com>
2020-09-21 15:06:42 -0400

    update

---
+++ js/langChange.js
@@ -0,0 +1,31 @@
+// Cambios de lenguaje 
+
+var language = localStorage.getItem('language');
+var default_lang = 'es';
+
+function setLanguage(lang) {
+    localStorage.setItem('language', lang);
+    language = localStorage.getItem('language');
+    getLanguage()
+}
+
+function getLanguage() {
+    (language == null) ? setLanguage(default_lang) : false;
+    $.ajax({
+        url: 'lang/' + language + '.json',
+        dataType: 'json', async: true
+    }).done(function (lang) {
+        $('html').attr('lang', language);
+        $.each(lang, function (index, val) {
+            (index === 'head') ? $(document).attr("title", val['title']) : false;
+            $(index).children().each(function () {
+                $(this).html(val[$(this).attr('key')])
+            })
+        })
+    })
+}
+
+$(document).ready(function () {
+    if (language != null && language !== default_lang)
+        getLanguage(language);
+});
